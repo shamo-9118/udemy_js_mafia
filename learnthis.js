c57ed189d4a002ecc191c162c20fddd75c28475c
shamo-9118 <won9118391183@gmail.com>
2022-12-09 06:39:34 +0900

    視聴分書いて試した！

--- learnthis.js
+++ learnthis.js
@@ -8,3 +8,36 @@
     }
     fn(arry);
 }
+{
+    const users = ["marumoto", "shoma", "yui", "deguchi"];
+    const hello = (propId) => {
+        setTimeout(() => {
+            console.log(`hello ${users[propId]}`);
+        }, 3000);
+    };
+    const showAlert = () => {
+        setTimeout(() => {
+            alert("Welcome");
+        }, 2000);
+    };
+    // hello(1);
+    // showAlert();
+}
+{
+    const name = "marumoto";
+    const iife = (function (name) {
+        console.log("colled iife");
+        const publicNum = 0;
+        const privateNum = 10;
+        function publicFunction() {
+            console.log(`called public ${name}`);
+        }
+        function privateFunction() {
+            console.log(`called private ${privateNum}`);
+        }
+        return {
+            publicFunction,
+            publicNum,
+        };
+    })(name);
+}
